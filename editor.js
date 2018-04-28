810ba1a867f2b08fe4ddc1d2110f47bc2b00476f
Dylan <>
2018-04-28 10:04:20 +1000

    a

--- editor.js
+++ editor.js
@@ -1,7 +1,7 @@
-window.addEventListener('load', function() {
-    setTimeout(function(){ 
+setTimeout(function(){
 
-    	var editor;
+
+var editor;
 
     ContentTools.StylePalette.add([
     new ContentTools.Style('Author', 'author', ['p'])
@@ -50,7 +50,56 @@ window.addEventListener('load', function() {
     xhr.send(payload);
 });
 
+}, 3000);
+window.addEventListener('load', function() {
+    
+    var editor;
 
-    }, 1000);
-   
+    ContentTools.StylePalette.add([
+    new ContentTools.Style('Author', 'author', ['p'])
+	]);
+	editor = ContentTools.EditorApp.get();
+	editor.init('*[data-editable]', 'data-name');
+
+	editor.addEventListener('saved', function (ev) {
+    var name, payload, regions, xhr;
+
+    // Check that something changed
+    regions = ev.detail().regions;
+    if (Object.keys(regions).length == 0) {
+        return;
+    }
+
+    // Set the editor as busy while we save our changes
+    this.busy(true);
+
+    // Collect the contents of each region into a FormData instance
+    payload = new FormData();
+    for (name in regions) {
+        if (regions.hasOwnProperty(name)) {
+            payload.append(name, regions[name]);
+        }
+    }
+
+    // Send the update content to the server to be saved
+    function onStateChange(ev) {
+        // Check if the request is finished
+        if (ev.target.readyState == 4) {
+            editor.busy(false);
+            if (ev.target.status == '200') {
+                // Save was successful, notify the user with a flash
+                new ContentTools.FlashUI('ok');
+            } else {
+                // Save failed, notify the user with a flash
+                new ContentTools.FlashUI('no');
+            }
+        }
+    };
+
+    xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', onStateChange);
+    xhr.open('POST', '/save-my-page');
+    xhr.send(payload);
 });
+
+}
